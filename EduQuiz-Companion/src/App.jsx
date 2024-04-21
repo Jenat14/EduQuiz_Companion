0a036978fd58362929cccdd5b1ae28e657b05b8f
Jenat14 <jenatcanil14@gmail.com>
2024-04-21 22:47:39 +0530

    Update App.jsx

--- EduQuiz-Companion/src/App.jsx
+++ EduQuiz-Companion/src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import './App.css'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import NavBar from "./components/NavBar";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Our Project</h1>
-    </>
-  )
+    <Router>
+      <NavBar /> {/* Render NavBar component */}
+      <div style={{ marginTop: "70px", padding: "20px" }}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+      </div>
+    </Router>
+ )
 }
-
-export default App
+export default App;

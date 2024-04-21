cf938dfcee499a6160d6aa9d1a227d3e398bb355
Jenat14 <jenatcanil14@gmail.com>
2024-04-21 22:55:56 +0530

    Update NavBar.jsx

---
+++ EduQuiz-Companion/src/components/NavBar.jsx
@@ -0,0 +1,48 @@
+import personIcon from "../assets/person-icon.png";
+import { Link } from "react-router-dom";
+function NavBar(){
+    return(
+        <nav className="navbar navbar-expand-lg  bg-light  fixed-top">
+          <div className="container-fluid">
+            <a className="navbar-brand" style={{fontWeight:"700",color:"#555555"}}>
+              <img src="#"></img> EduQuiz.
+            </a>
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+                <Link className="nav-link"  to="#" style={{color:"#555555",fontWeight:600}}>
+                  Home
+                </Link>
+              </li>
+              <li id="dropdowns" className="nav-items">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                >
+                  <img src={personIcon} style={{height:"25px", width:"25px"}}></img>
+                </a>
+                <ul className="dropdown-menu dropdown-menu-end">
+                  <li>
+                    <a className="dropdown-item disabled" aria-disabled="true" href="#">
+                      Name
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Change Password
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Logout
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </nav>
+    )
+}
+export default NavBar;

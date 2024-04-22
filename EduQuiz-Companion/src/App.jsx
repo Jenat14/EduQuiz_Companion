b4cfe0c770ecb84f4a84bbfb27618759f0595362
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-04-23 00:17:49 +0530

    App.jsx updation

--- EduQuiz-Companion/src/App.jsx
+++ EduQuiz-Companion/src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import NavBar from "./components/NavBar";
+import Subject from "./pages/Subject";
+import LevelPage from "./pages/LevelPage";
+import Leaderboard from "./pages/Leaderboard";
 function App() {
   return (
     <Router>
@@ -8,6 +11,9 @@ function App() {
       <div style={{ marginTop: "70px", padding: "20px" }}>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/Subject" element={<Subject />} />
+        <Route path="/levelPage" element={<LevelPage />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
       </Routes>
       </div>
     </Router>

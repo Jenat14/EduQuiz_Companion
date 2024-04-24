825f7843da10b716eb036b9e5e48477d1686ccb0
Jenat14 <jenatcanil14@gmail.com>
2024-04-25 01:20:47 +0530

    Update loginS.jsx

--- EduQuiz-Companion/src/pages/loginS.jsx
+++ EduQuiz-Companion/src/pages/loginS.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-
-import "../logins.css";
+import '../logins.css';
 import img1 from '../assets/student_login.jpg';
 
 const LoginStudent = () => {
@@ -15,24 +14,22 @@ const LoginStudent = () => {
   };
 
   return (
-    <div className="login-container">
+    <div className="login-container" styles={{height:"100vh"}}>
       <div className="left-side">
         <img src={img1} alt="Logo" className="logo-image" />
       </div>
       <div className="right-side">
         <b>
-        <h1 className="xyz">Login to Your Student Account</h1>
+          <h2 className="xyz">Login to Your Student Account</h2>
         </b>
-        <form onClick={handleLogin}>
-          <div className="form-group">
-            <label htmlFor="username">Username:</label>
+        <form onSubmit={handleLogin}>
+          <div className="form-group" style={{marginBottom:"30px"}}>
             <input type="text" id="username" name="username" placeholder="Enter your username" />
           </div>
-          <div className="form-group">
-            <label htmlFor="password">Password:</label>
+          <div className="form-group" style={{marginBottom:"30px"}}>
             <input type="password" id="password" name="password" placeholder="Enter your password" />
           </div>
-          <button type="button" onClick={handleLogin}>Login</button>
+          <button type="submit" style={{marginTop:"30px"}}>Login</button>
         </form>
         <div className="forgot-password-link" onClick={handleForgotPassword}>
           <a href="#">Forgot password?</a>
@@ -43,4 +40,3 @@ const LoginStudent = () => {
 };
 
 export default LoginStudent;
-

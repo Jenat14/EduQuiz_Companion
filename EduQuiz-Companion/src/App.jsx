59f46c5a0a33e4cb51d9ddd3227a15bc2db8722c
Jenat14 <jenatcanil14@gmail.com>
2024-04-24 21:08:32 +0530

    Update App.jsx

--- EduQuiz-Companion/src/App.jsx
+++ EduQuiz-Companion/src/App.jsx
@@ -7,6 +7,7 @@ import Leaderboard from "./pages/Leaderboard";
 import LoginStudent from "./pages/loginS";
 import StudentSub from "./pages/StudentSub";
 import CardPage from "./pages/Sdashboard";
+import Result from "./pages/Result";
 function App() {
   return (
     <Router>
@@ -20,6 +21,7 @@ function App() {
         <Route path="/leaderboard" element={<Leaderboard />} />
         <Route path="/StudentSub" element={<StudentSub />} />
         <Route path="/CardPage" element={<CardPage />} />
+        <Route path="/Result" element={<Result />} />
       </Routes>
       </div>
     </Router>

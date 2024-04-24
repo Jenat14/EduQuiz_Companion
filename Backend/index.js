dcef5dba29efa2e20d3f3a784382715f7e1d8a35
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-04-24 22:35:22 +0530

    backend updation

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,26 @@
 // Example using Express.js
 const express = require('express');
 const app = express();
+const admin = require('firebase-admin');
+
+// Initialize Firebase Admin SDK
+const serviceAccount = require('../Firebase/service-account.json'); // Path to your service account key file
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 // Example defining a route in Express
 app.get('/', (req, res) => {
     res.send('<h1>Hello hi, Express.js Server!</h1>');
 });
+
+/*
+app.use("/auth",authROute)
+
+auth.routes.js
+app.get(/logn, getStudentData)
+app.)
+*/
 
 // Example specifying the port and starting the server
 const port = process.env.PORT || 3000; // You can use environment variables for port configuration

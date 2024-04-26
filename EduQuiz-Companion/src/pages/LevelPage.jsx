6e70e5aad898d73161771bf0370f9b2cc07915f4
Jenat14 <jenatcanil14@gmail.com>
2024-04-27 01:53:59 +0530

    Update LevelPage.jsx

--- EduQuiz-Companion/src/pages/LevelPage.jsx
+++ EduQuiz-Companion/src/pages/LevelPage.jsx
@@ -9,15 +9,15 @@ function LevelPage() {
                     <h2 style={{ margin: 0,color:"#222831" }}>LEVEL {Levelno}</h2>
                 </div>
                 <div className="col-md-6 d-grid d-md-flex justify-content-md-end">
-                    <button className="btn btn-primary" style={{ borderRadius: "25px", backgroundColor:"#76ABAE", border: "1px solid #76ABAE", color: "#FFFDFD",width: "200px" }}>
+                <Link to="/PageLayout"><button className="btn btn-primary" style={{ borderRadius: "25px", backgroundColor:"#76ABAE", border: "1px solid #76ABAE", color: "#FFFDFD",width: "200px" }}>
                     <h6>Add New Quiz</h6>
-                    </button>
+                    </button></Link>
                 </div>
             </div>
             {/*correct here*/}
             <div style={{padding:"5%"}}>
             {list.map((quiz, index) => (
-                <Link to="" style={{ textDecoration: 'none' }}>
+                <Link to="#" style={{ textDecoration: 'none' }}>
                 <div className="card mb-3 shadow-bottom" style={{ width:"70rem",backgroundColor:"#EEEEEE",borderColor:"#76ABAE"}} key={index}>
                     <div className="card-body d-flex align-items-center justify-content-center">
                         <h5 className="card-title" style={{color:"#222831"}}>{quiz}</h5>

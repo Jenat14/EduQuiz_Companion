0f75bad30ae360cda4c88ab11589564d7ec6fdd5
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-04-27 00:46:22 +0530

    Faculty login frontend

--- EduQuiz-Companion/src/pages/Facultylogin.jsx
+++ EduQuiz-Companion/src/pages/Facultylogin.jsx
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 
 import "../Facultylogin.css";
 import img1 from '../assets/facultylogin.png';
 
 const Facultylogin = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
   
   const navigate = useNavigate();
-  const handleLogin = () => {
-    // Handle login logic here
-    console.log('Logging in...'); 
-    navigate("/facultydash");
+  const handleLogin = async (e) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('http://localhost:3000/facultylogin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ id: username, password }),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error);
+      }
+
+      // Login successful, handle the response data
+      const userData = await response.json();
+      console.log('Login successful:', userData);
+      // Redirect to the dashboard or whatever page you want
+      window.location.href = "/cardpage";
+
+    } catch (error) {
+      setErrorMessage(error.message);
+      // Clear error message after 3 seconds
+      setTimeout(() => {
+        setErrorMessage('');
+      }, 3000);
+    }
   };
 
   const handleForgotPassword = () => {
@@ -30,12 +58,13 @@ const Facultylogin = () => {
         <form onClick={handleLogin}>
           <div className="form-group">
             <label htmlFor="username">Username:</label>
-            <input type="text" id="username" name="username" placeholder="Enter your username" />
+            <input type="text" id="username" name="username" placeholder="Enter your username" onChange={(e) => setUsername(e.target.value)}  />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password" placeholder="Enter your password" />
+            <input type="password" id="password" name="password" placeholder="Enter your password" onChange={(e) => setPassword(e.target.value)}  />
           </div>
+          {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
           <button type="button" onClick={handleLogin} style={{marginTop:"30px"}}>Login</button>
         </form>
         <div className="forgot-password-link" onClick={handleForgotPassword}>

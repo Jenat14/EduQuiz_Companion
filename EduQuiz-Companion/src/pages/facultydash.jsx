23bf6689b1f22bf522394e9edbf4517845ca86eb
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-04-27 01:21:53 +0530

    Faculty dashboard linking

--- EduQuiz-Companion/src/pages/facultydash.jsx
+++ EduQuiz-Companion/src/pages/facultydash.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import "../Facultydash.css"; // Import your CSS file here
 import img2 from '../assets/os.jpeg'; // Import your image here
 import img3 from '../assets/dsa.jpg';
@@ -13,45 +14,55 @@ const Facultydash = () => {
 <h1><b>FACULTY DASHBOARD</b></h1>
 <button className='top-right-button'> Add New Subject </button>
 </div>
-        
+       
     <div className="card-container-f">
-  
+      
       <div className="card-row-f">
         <div className="card-f">
           <img src={img2} alt="Card" className="card-image-f" />
+          <Link to="/LevelPage" style={{textDecoration:"none"}}>
           <div className="card-content-f">
             <button className="card-button"><b>Operating Systems</b></button>
           </div>
+          </Link>
         </div>
-
+    
         <div className="card-f">
           <img src={img3} alt="Card" className="card-image-f" />
+          <Link to="/LevelPage" style={{textDecoration:"none"}}>
           <div className="card-content-f">
             <button className="card-button" ><b>Data Structures</b></button>
           </div>
+          </Link>
         </div>
 
         <div className="card-f">
           <img src={img4} alt="Card" className="card-image-f" />
+          <Link to="/LevelPage" style={{textDecoration:"none"}}>
           <div className="card-content-f">
             <button className="card-button"><b>Database Management System</b></button>
           </div>
+          </Link>
         </div>
       </div>
 
       <div className="card-row-f">
         <div className="card-f">
           <img src={img5} alt="Card" className="card-image-f" />
+          <Link to="/LevelPage" style={{textDecoration:"none"}}>
           <div className="card-content-f">
             <button className="card-button"><b>Computer Organization And Architecture</b></button>
           </div>
+          </Link>
         </div>
 
         <div className="card-f">
           <img src={img6} alt="Card" className="card-image-f" />
+          <Link to="/LevelPage" style={{textDecoration:"none"}}>
           <div className="card-content-f">
             <button className="card-button"><b>Formal Languages And Automata Theory</b></button>
           </div>
+          </Link>
         </div>
       </div>
     </div>

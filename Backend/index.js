c67ba6d4b3ecf421c7530643a064dab96324c30e
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-04-27 11:22:59 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -8,16 +8,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
-// Initialize Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: '"https://my-project-EduQuiz-Companion.firebasedatabase.app"' // Replace with your Firebase database URL
+  databaseURL: '"https://my-project-EduQuiz-Companion.firebasedatabase.app"' 
 });
 
-// Get a reference to the Firebase database
-const db = admin.firestore();
+const db = admin.firestore();//reference to the Firebase database
 
-// Define a route to fetch data from the "student" collection
 app.get('/student', async (req, res) => {
   try {
     const studentsSnapshot = await db.collection('Student').get();
@@ -43,17 +40,16 @@ app.post('/login', async (req, res) => {
     if (!id || !password) {
       return res.status(400).json({ error: 'ID and password are required' });
     }
-    // Query the database for the user with the provided ID
+    
     const userSnapshot = await db.collection('Student').doc(id).get();
     if (!userSnapshot.exists) {
       return res.status(404).json({ error: 'User not found' });
     }
     const userData = userSnapshot.data();
-    // Check if the provided password matches the stored password
     if (userData.password !== password) {
       return res.status(401).json({ error: 'Invalid password' });
     }
-    // If authentication is successful, send a success response
+   
     res.json({ message: 'Login successful', user: userData });
   } catch (error) {
     console.error('Error authenticating user:', error);
@@ -66,17 +62,15 @@ app.post('/facultylogin', async (req, res) => {
     if (!id || !password) {
       return res.status(400).json({ error: 'ID and password are required' });
     }
-    // Query the database for the user with the provided ID
+    
     const userSnapshot = await db.collection('Faculty').doc(id).get();
     if (!userSnapshot.exists) {
       return res.status(404).json({ error: 'User not found' });
     }
     const userData = userSnapshot.data();
-    // Check if the provided password matches the stored password
     if (userData.Password !== password) {
       return res.status(401).json({ error: 'Invalid password' });
     }
-    // If authentication is successful, send a success response
     res.json({ message: 'Login successful', user: userData });
   } catch (error) {
     console.error('Error authenticating user:', error);

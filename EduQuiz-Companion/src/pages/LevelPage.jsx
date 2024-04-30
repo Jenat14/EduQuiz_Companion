608819635d078d086465b033e305fd25753356b5
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-04-30 21:47:09 +0530

    Update LevelPage.jsx

--- EduQuiz-Companion/src/pages/LevelPage.jsx
+++ EduQuiz-Companion/src/pages/LevelPage.jsx
@@ -19,7 +19,7 @@ function LevelPage() {
         <div style={{marginTop:"70px",padding:"5%"}}>
             <div className="row">
                 <div className="col-md-6 ">
-                    <h2 style={{ margin: 0,color:"#222831" }}>LEVEL {Levelno}</h2>
+                    <h2 style={{ margin: 0,color:"#222831" }}>LEVEL { new URLSearchParams(location.search).get("level")}</h2>
                 </div>
                 <div className="col-md-6 d-grid d-md-flex justify-content-md-end">
                 <Link to="/PageLayout"><button className="btn btn-primary" style={{ borderRadius: "25px", backgroundColor:"#76ABAE", border: "1px solid #76ABAE", color: "#FFFDFD",width: "200px" }}>

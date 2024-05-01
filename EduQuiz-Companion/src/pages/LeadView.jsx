e5ae59594bc6a4a36c275229c18783e2c02ef957
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-05-01 09:34:41 +0530

    Update LeadView.jsx

--- EduQuiz-Companion/src/pages/LeadView.jsx
+++ EduQuiz-Companion/src/pages/LeadView.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import "../LeadView.css"; // Import CSS file for custom styling
 import { Link } from 'react-router-dom';
+
 const timeStyle = {
   display: 'flex',
   justifyContent: 'space-between',
@@ -26,30 +27,8 @@ const inputStyle = {
 const paragraphStyle = {
   margin: '5px', // Remove default margins
 };
- 
- const questionsData = [
-  {
-    question: "Question 1",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
-    marks: 10,
-    correctAnswer: 2,
-  },
-  {
-    question: "Question 2",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
-    marks: 2,
-    correctAnswer: 3,
-  },
-  {
-    question: "Question 3",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
-    marks: 5,
-    correctAnswer: 1,
-  },
-  // Add more questions here as needed
-];
+
 const LeadView = () => {
- 
   const [quizDetails, setQuizDetails] = useState(null);
   const [questions, setQuestions] = useState([]);
   const [error, setError] = useState(null);
@@ -59,7 +38,7 @@ const LeadView = () => {
       try {
         const searchParams = new URLSearchParams(window.location.search);
         const quizParam = searchParams.get('quiz');
-  
+
         if (!quizParam) {
           throw new Error('No quiz parameter found in the URL');
         }
@@ -75,11 +54,10 @@ const LeadView = () => {
           throw new Error('Failed to fetch quiz details');
         }
         const quizData = await quizResponse.json();
-  
+
         // Set quiz details state
         setQuizDetails(quizData);
-        console.log(quizDetails);
-  
+
         // Fetch questions
         const questionsResponse = await fetch('http://localhost:3000/quizdata/questions', {
           method: 'POST',
@@ -112,69 +90,86 @@ const LeadView = () => {
     return <div>Loading...</div>;
   }
 
-
   return (
     <>
       <div className="page" style={{marginTop:"70px"}}>
-      <div className="top-bar bg-teal text-white py-2" style={{height:"150px"}} >
-        <div className="container-fluid">
-          <div className="row align-items-center" style={{paddingTop:"30px"}}>
-            <div className="col-12 col-md-4">
-              <h2>OPERATING SYSTEMS</h2>
-            </div>
-            <Link to="/Leaderboard"><div className=" finish-btn  text-right">
-              <button className="btn btn-light"style={{position:"absolute",right:"60px",top:"180px",padding:"10px",borderRadius:"20px",width:"200px"}}>View Leaderboard</button>
-            </div></Link>
+        <div className="top-bar bg-teal text-white py-2" style={{height:"150px"}} >
+          <div className="container-fluid">
+            <div className="row align-items-center" style={{paddingTop:"30px"}}>
+              <div className="col-12 col-md-4">
+                <h2>OPERATING SYSTEMS</h2>
+              </div>
+              <Link to="/Leaderboard">
+                <div className=" finish-btn  text-right">
+                  <button className="btn btn-light" style={{position:"absolute",right:"60px",top:"180px",padding:"10px",borderRadius:"20px",width:"200px"}}>View Leaderboard</button>
+                </div>
+              </Link>
 
-            <div className="level-info" >
-              <br></br>
+              <div className="level-info" >
+                <br />
                 <h3>Level: {quizDetails.level}</h3>
               </div>
             
+            </div>
           </div>
         </div>
-      </div>
-      <div >
-      <div style={{paddingTop:"30px"}}>Time Limit:{quizDetails.time} min</div>
-      <div style={rightAlign}>
-      <div style={{paddingRight:"30px"}}>Total Questions: {quizDetails.numberOfQuestions}</div>
-      <div style={{paddingRight:"30px"}}>Maximum Mark: {quizDetails.totalMarks}</div>
-      </div>
-    </div>
+        <div >
+          <div style={{paddingTop:"30px"}}>Time Limit: {quizDetails.time} min</div>
+          <div style={rightAlign}>
+            <div style={{paddingRight:"30px"}}>Total Questions: {quizDetails.numberOfQuestions}</div>
+            <div style={{paddingRight:"30px"}}>Maximum Mark: {quizDetails.totalMarks}</div>
+          </div>
+        </div>
 
-      {/* Main Quiz Section */}
-      <div className="main-content py-4">
-        <div className="container">
-          {/* Map through questionsData array to render questions and options */}
-          {questionsData.map((questionObj, index) => (
-            <div key={index} className="quiz-box"style={{backgroundColor:"#F7FCFC",borderColor:"#76ABAE"}}>
-              <div className="row">
-                <div className="col-8">
-                  <h5>{questionObj.question}</h5>
+        {/* Main Quiz Section */}
+        <div className="main-content py-4">
+          <div className="container">
+            {/* Map through questions array to render questions and options */}
+            {questions.map((questionObj, index) => (
+              <div key={index} className="quiz-box"style={{backgroundColor:"#F7FCFC",borderColor:"#76ABAE"}}>
+                <div className="row">
+                  <div className="col-8">
+                    <h5>{questionObj.question}</h5>
+                  </div>
+                  <div className="col-4" style={{textAlign:"end"}}>
+                    <p>Marks: {questionObj.mark}</p>
+                  </div>
                 </div>
-                <div className="col-4" style={{textAlign:"end"}}>
-                  <p>Marks: {questionObj.marks}</p>
-                </div>
-              </div>
-              <div className="row">
-                {questionObj.options.map((option, optionIndex) => (
-                  <div key={optionIndex} className="col-6 option">
-                    <label htmlFor={`q${index + 1}_option${optionIndex + 1}`} style={labelStyle}>
-                    <input type="radio" id={`q${ index + 1 }_option${ optionIndex + 1 }`} name={`q${ index + 1 }`} value={`option${ optionIndex + 1 }`} style={{ width: '15px', margin: '5px' }} disabled />
-                      <p style={paragraphStyle}>{option}</p>
+                <div className="row">
+                  <div className="col-6 option">
+                    <label htmlFor={`q${index + 1}_option1`} style={labelStyle}>
+                      <input type="radio" id={`q${ index + 1 }_option1`} name={`q${ index + 1 }`} value="option1" style={inputStyle} disabled />
+                      <p style={paragraphStyle}>{questionObj.option1}</p>
                     </label>
                   </div>
-                ))}
-              </div>
-              <div className="row">
-                  <div className="col-12" style={{ textAlign:"left" }}>
-                    <p style={{ margin: "10px", padding: "5px" }}>Correct Answer: {questionObj.options[questionObj.correctAnswer]}</p>
+                  <div className="col-6 option">
+                    <label htmlFor={`q${index + 1}_option2`} style={labelStyle}>
+                      <input type="radio" id={`q${ index + 1 }_option2`} name={`q${ index + 1 }`} value="option2" style={inputStyle} disabled />
+                      <p style={paragraphStyle}>{questionObj.option2}</p>
+                    </label>
+                  </div>
+                  <div className="col-6 option">
+                    <label htmlFor={`q${index + 1}_option3`} style={labelStyle}>
+                      <input type="radio" id={`q${ index + 1 }_option3`} name={`q${ index + 1 }`} value="option3" style={inputStyle} disabled />
+                      <p style={paragraphStyle}>{questionObj.option3}</p>
+                    </label>
+                  </div>
+                  <div className="col-6 option">
+                    <label htmlFor={`q${index + 1}_option4`} style={labelStyle}>
+                      <input type="radio" id={`q${ index + 1 }_option4`} name={`q${ index + 1 }`} value="option4" style={inputStyle} disabled />
+                      <p style={paragraphStyle}>{questionObj.option4}</p>
+                    </label>
                   </div>
                 </div>
-            </div>
-          ))}
+                <div className="row">
+                  <div className="col-12" style={{ textAlign:"left" }}>
+                    <p style={{ margin: "10px", padding: "5px" }}>Correct Answer: {questionObj.correctAnswer}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
       </div>
     </>
   );

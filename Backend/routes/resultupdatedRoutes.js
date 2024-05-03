6afaf1d685427a2620aa8368d226ae59e24eea1d
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-05-03 16:25:59 +0530

    Update resultupdatedRoutes.js

--- Backend/routes/resultupdatedRoutes.js
+++ Backend/routes/resultupdatedRoutes.js
@@ -6,7 +6,6 @@ router.post('/', async (req, res) => {
     try {
         const { studentId, quizId, score, timestamp, timeTaken } = req.body;
         let noofattempts=1;
-        // Check if a document exists with the given studentId and quizId
         const querySnapshot = await db.collection('results')
             .where('studentId', '==', studentId)
             .where('quizId', '==', quizId)
@@ -16,9 +15,7 @@ router.post('/', async (req, res) => {
             
             const existingResult = querySnapshot.docs[0].data();
             noofattempts=existingResult.noofattempts+1;
-            console.log(noofattempts);
             if (score > existingResult.score) {
-                // Update the score
                 await db.collection('results')
                     .doc(querySnapshot.docs[0].id)
                     .update({
@@ -34,12 +31,9 @@ router.post('/', async (req, res) => {
                     .update({
                         noofattempts
                     });
-                // Score not greater, return a message
                 res.status(200).json({ message: 'Score is not greater, noofattempts updated needed' });
             }
         } else {
-            // Document does not exist, create a new one
-            
             await db.collection('results').add({
                 studentId,
                 quizId,

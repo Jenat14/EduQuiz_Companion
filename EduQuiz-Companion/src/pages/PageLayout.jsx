5d8d8c459c139507faf1adba7c82dec485213feb
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-05-04 23:49:19 +0530

    Update PageLayout.jsx

--- EduQuiz-Companion/src/pages/PageLayout.jsx
+++ EduQuiz-Companion/src/pages/PageLayout.jsx
@@ -12,7 +12,7 @@ const PageLayout = () => {
   const [quizData, setQuizData] = useState(null);
 
   const handleDownloadTemplate = () => {
-    const templateUrl = "http://localhost:5173/public/Book1.xlsx"; 
+    const templateUrl = "http://localhost:5173/Book1.xlsx"; 
     setFileDownloadUrl(templateUrl);
   };
   const rightAlign = {

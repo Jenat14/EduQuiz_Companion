f4f16e488015979ece320d83b6b88e176d6de157
albinsajijohn <albinsajijohn@gmail.com>
2024-05-06 18:33:41 +0530

    Update Question.jsx

--- EduQuiz-Companion/src/pages/Question.jsx
+++ EduQuiz-Companion/src/pages/Question.jsx
@@ -240,12 +240,15 @@ useEffect(() => {
   if (!quizDetails || questions.length === 0) {
     return (
       <div style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "100vh" }}>
-  <div className="text-center">
-    <div className="spinner-grow" role="status" style={{ width: "6rem", height: "6rem",color:"#212529", animationDuration: "1s" }}>
+      <div className="text-center" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div className="spinner-grow" role="status" style={{ width: "2rem", height: "2rem", color: "#222831", marginRight: "1rem", animationDuration: "1s", animationDelay: "0s" }} />
+          <div className="spinner-grow" role="status" style={{ width: "2rem", height: "2rem", color: "#2D5D81", marginRight: "1rem", animationDuration: "1s", animationDelay: "0.25s" }} />
+          <div className="spinner-grow" role="status" style={{ width: "2rem", height: "2rem", color: "#76ABAE", animationDuration: "1s", animationDelay: ".5s" }} />
+        </div>
+        <p style={{ color: "#222831", marginTop: "1rem",fontFamily:"monospace" }}>Loading...</p>
+      </div>
     </div>
-    <p style={{color:"#212529"}}>Loading...</p>
-  </div>
-</div>
     );
   }
 
